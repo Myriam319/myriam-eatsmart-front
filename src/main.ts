aae9947eb47234afca7025a35715e78f0c453933
Myriam319 <bmyriam990@gmail.COM>
2026-03-11 06:10:26 -0700

    liste des plats

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import './style.css'
 
 interface Commande {
     id:number,
@@ -6,49 +7,22 @@ interface Commande {
     description: string[],
     
 } 
-let maCommande: Commande = {
-    id: 1,
-    nom: "Anchois 23cm",
-    prix:7.9,
-    description : ["sauce tomate premium, origan,huile d'olive extra vierge,anchois,olive"],
-    
-};
-let maCommande1: Commande = {
-    id: 2,
-    nom: "Emmental 23cm",
-    prix:7.9,
-    description : ["sauce tomate premium, origan,huile d'olive extra vierge,emmental,basilic,olive"],
-    
-};
-let maCommande2: Commande = {
-    id: 3,
-    nom: "Margherita 23cm",
-    prix:7.9,
-    description : ["sauce tomate premium, origan,huile d'olive extra vierge,emmental,basilic,olive"],
-    
-};
 
 
-console.log(` 1: 
-    descrption: "${maCommande.description} "
-    id: ${maCommande.id} 
-    nom: ${maCommande.nom}
-    prix: ${maCommande.prix}   `)
-
-    console.log(` 2: 
-    descrption: "${maCommande1.description} "
-    id: ${maCommande1.id} 
-    nom: ${maCommande1.nom}
-    prix: ${maCommande1.prix}   `)
-
-    console.log(` 3: 
-    descrption: "${maCommande2.description} "
-    id: ${maCommande2.id} 
-    nom: ${maCommande2.nom}
-    prix: ${maCommande2.prix}   `)
+const carte: Plat[] = [
+    {id:0,nom: "Anchois 23cm", prix: 7.9, description:"sauce tomate premium, origan,huile d'olive extra vierge,anchois,olive"},
+    {id:1,nom: "Emmental 23cm", prix: 7.9, description:"sauce tomate premium, origan,huile d'olive extra vierge,emmental,basilic,olive"},
+    {id:2,nom: "Margherita 23cm", prix: 7.9, description:"sauce tomate premium, origan,huile d'olive extra vierge,emmental,basilic,olive" },
+];
 
 
 
-
-
+carte.forEach((plat, index) => {
+    console.log(`${index + 1}:
+    description: ${plat.description}
+    id: ${plat.id}
+    nom: ${plat.nom}
+    prix ${plat.prix}`);
+         
+});
 
